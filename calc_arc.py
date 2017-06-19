2e0c52ccd7ec891580bc478d563054241eefb937
Varun Iyer <vi.mail@protonmail.ch>
2017-06-19 12:52:56 -0700

    Geometry file with Plane, Vector, and Class

--- calc_arc.py
+++ calc_arc.py
@@ -3,129 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as ptch
 import numpy as np
 from random import uniform
-from math import isnan
-
-#TODO vector class
-
-def slope(*points):
-    return(points[0][1] - points[1][1]) / (points[0][0] - points[1][0])
-
-
-def midpoint(*points):
-    return (points[0][0] + points[1][0]) * .5, (points[0][1] + points[1][1]) * .5
-
-
-def dist(p1, p2):
-    total = 0
-    for v1, v2 in zip(p1, p2):
-        total += (v1 - v2) ** 2
-    return total ** .5
-
-
-def vec_add(p1, p2):
-    newvec = []
-    for v1, v2 in zip(p1, p2):
-        newvec.append(v1 + v2)
-    return tuple(newvec)
-
-
-def scalar_multiply(pt, scalar):
-    return tuple([pt * i for i in scalar])
-
-def collinear(*args):
-    if args[0][0] == args[1][0]:
-        for x, y in args:
-            if x != args[0][0]:
-                return False
-    m = (args[0][1] - args[1][1]) / (args[0][0] - args[1][0])
-    b = args[0][1] - (m * args[0][0])
-    for x, y in args:
-        if round(y, 5) != round(m*x + b, 5):
-            return False
-    return True
-
-
-def cross_product(vec1, vec2):
-    return [vec1[1] * vec2[2] - vec1[2] * vec2[1],
-            -1 * (vec1[0] * vec2[2] - vec1[2] * vec2[0]),
-            vec1[0] * vec2[1] - vec1[1] * vec2[0]]
-
-
-class Circle(object):
-    def __str__(self):
-        return "radius:{} center:{}".format(self.radius, self.center)
-
-    def __init__(self, *args):
-        if len(args) == 3 and hasattr(args[1], '__iter__'):
-            """ Complex solution for the radius of a circle from 3 points returns radius, centerx, centery """
-            if collinear(*args):
-                raise ValueError("Points used to init circle are collinear")
-                return
-            p1, p2, p3 = [complex(*p) for p in args]
-            for i in range(3):
-                try:
-                    p1, p2, p3 = p3, p1, p2
-                    diff = p3 - p1
-                    diff /= p2 - p1
-                    center = (p1 - p2) * (diff - abs(diff) **2) / 2j / diff.imag - p1
-                    self.center = center.real, center.imag
-                except ZeroDivisionError:
-                    pass
-            self.radius = abs(complex(*self.center) + p1)
-            self.x = self.center[0]
-            self.y = self.center[1]
-        elif len(args) == 3:
-            self.radius = args[0]
-            self.x = args[1]
-            self.y = args[2]
-            self.center = (self.x, self.y)
-        else:
-            self.radius = args[0]
-            self.center = args[1]
-            self.x = self.center[0]
-            self.y = self.center[1]
-
-    def chord_angle(self, chord):
-        return 2 * np.arcsin(chord/(2*self.radius))
-
-    def intersection(self, other):
-        """ 
-        returns the points of overlap between 2 circles at a given distance
-        """
-        d = dist(other.center, self.center)
-        if self.center == other.center:
-            raise ValueError("Circles have the same center")
-            return
-        if d > self.radius + other.radius:
-            raise ValueError("Circles do not touch")
-            return
-        mid_dst = (self.radius **2 - other.radius **2 + d **2) / (2 * d)
-        relative_pt = [p * mid_dst / d for p in vec_add(other.center, [-1 * i for i in self.center])]
-        mid_pt = vec_add(self.center, relative_pt)
-        chord_len = 2 * (abs(self.radius ** 2 - mid_dst ** 2)) ** .5
-        slope = chord_len / (2 * d)
-        inter_pt = mid_pt[0] + slope * (other.y - self.y), mid_pt[1] - slope * (other.x - self.x)
-        inter_pt2 = mid_pt[0] - slope * (other.y - self.y), mid_pt[1] + slope * (other.x - self.x)
-        return inter_pt, inter_pt2
-
-    def arc_length(self, p1, p2):
-        central_angle = self.chord_angle(dist(p1, p2))
-        return central_angle * self.radius
-
-    def plt_circle(self, color='blue'):
-        return plt.Circle(self.center, self.radius, color=color)
-
-
-def Plane(object):
-
-    def __init__(self, *args):
-        if len(args) == 3 and all(hasattr(p, '__iter__') and len(p) == 3 for p in args):
-            vec1 = vec_add(args[1], scalar_multiply(args[0], -1))
-            vec2 = vec_add(args[2], scalar_multiply(args[0], -1))
-            c_p = cross_product(vec1, vec2)
-
-            self.a, self.b, self.c = c_p
-            self.d = c_p[0] * args[0][1] + c_p[1] * args[0][1] + c_p[2] * args[0][2]
+from geometry import Plane, Vector, Circle, Point
 
 
 def calc_times(emitters, middles, detectors, sphere_radius, ref_index):
@@ -145,9 +23,9 @@ def calc_times(emitters, middles, detectors, sphere_radius, ref_index):
         for middle in middles:
             for end in detectors:
 
-                plane = Plane(start, middle, end)
+                #plane = Plane(start, middle, end)
 
-                if collinear(start, middle, end):
+                if Point.collinear(start, middle, end):
                     continue
 
                 # sometimes it doesn't get that points are collinear because of floating point misses
@@ -158,7 +36,8 @@ def calc_times(emitters, middles, detectors, sphere_radius, ref_index):
                 small = Circle(sphere_radius, (0, 0))
 
                 inter_pts = large.intersection(small)
-                e_pt, r_pt = min(inter_pts, key=lambda x: dist(x, start)),  max(inter_pts, key=lambda x: dist(x, start))
+                e_pt, r_pt = (min(inter_pts, key=lambda x: x.distance(start)),
+                              max(inter_pts, key=lambda x: x.distance(start)))
 
                 l1 = large.arc_length(start, e_pt)
                 l2 = large.arc_length(e_pt, r_pt)
@@ -167,17 +46,15 @@ def calc_times(emitters, middles, detectors, sphere_radius, ref_index):
     return times
 
 # so maybe I've gone sliiiightly overboard with those list comprehensions
-# times = [( i[1].center, i[1].radius * 2, i[1].radius * 2, 0, np.arctan(slope(i[2][0], i[0].center)),
-        #np.arctan(slope(i[1][0], i[0].center)), ((20-sum(i[0]/20)) for i in
-
-times = calc_times([(-10, 0)], [(0, uniform(-5, 5)) for i in range(50)], [(10, uniform(-5, 5)) for i in range(50)], 6, .2)
+times = calc_times([Point(-10, 0)], [Point(0, uniform(-5, 5)) for i in range(50)], [Point(10, uniform(-5, 5))
+                                                                                    for i in range(50)], 6, .2)
 fig, ax = plt.subplots()
 ax.set_xlim((-10, 10))
 ax.set_ylim((-10, 10))
 max_time = max([sum(t[0]) for t in times])
 for time, circle, inter_pts in times:
     time = sum(time)
-    angles = (np.arctan(slope(p, circle.center)) for p in inter_pts)
+    angles = (np.arctan(p.slope(circle.center)) for p in inter_pts)
     color = (time/ max_time, (max_time - time) / max_time, 0, .1)
     ax.add_patch(ptch.Arc(circle.center, circle.radius * 2, circle.radius * 2, 0, *angles, edgecolor=color))
 
---
+++ geometry.py
@@ -0,0 +1,189 @@
+import vectors as v
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Vector(object):
+    """ 
+    I didn't mean to create a whole new vector class, I was using the one from vectors as well as a bunch of random
+    helper functions from the beginning, and then I noticed the vectors.vector class was really bad an made an 
+    improvement or two, and the next thing you know it's its own object
+    """
+    def __init__(self, *args):
+        self.iterable = args
+        self.x = self.iterable[0]
+        self.y = self.iterable[1]
+        self.z = self.iterable[1]
+
+    def slope(*points):
+        points = list(points)
+        for idx, p in enumerate(points):
+            if not isinstance(p, v.Point):
+                points[idx] = v.Point(*p, 0)
+        return(points[0].y - points[1].y) / (points[0].x - points[1].x)
+
+    def midpoint(*points):
+        points = list(points)
+        for idx, p in enumerate(points):
+            if not isinstance(p, v.Point):
+                points[idx] = v.Point(*p, 0)
+        return (points[0].x + points[1].x) * .5, (points[0].y + points[1].y) * .5
+
+    def distance(self, p2):
+        total = 0
+        for v1, v2 in zip(self, p2):
+            total += (v1 - v2) ** 2
+        return total ** .5
+
+    def collinear(*args):
+        if args[0].x == args[1].x:
+            for x, y in args:
+                if x != args[0].x:
+                    return False
+        m = (args[0].y - args[1].y) / (args[0].x - args[1].x)
+        b = args[0].y - (m * args[0].x)
+        for x, y in args:
+            if round(y, 5) != round(m*x + b, 5):
+                return False
+        return True
+
+    def __getitem__(self, n):
+        return self.iterable.__getitem__(n)
+
+    def __iter__(self):
+        return self.iterable.__iter__()
+
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(*[s + o for s, o in zip(self, other)])
+        else:
+            return Vector(*[i + other for i in self])
+
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            total = 0
+            for s, o in zip(self, other):
+                total += s * o
+            return total
+        else:
+            return Vector(*[i * other for i in self])
+
+    def __sub__(self, other):
+        return self + (other * -1)
+
+    def __truediv__(self, other):
+        return self * (other ** -1)
+
+    def __matmul__(self, other):
+        # Too lazy to do cross products myself
+        return Vector(*v.Vector.from_list(self.iterable).cross(v.Vector.from_list(other.iterable)).vector)
+
+    def __abs__(self):
+        return self.distance(Vector(*[0 for i in self.iterable]))
+
+    def __str__(self):
+        return str("<{}>".format(str(self.iterable)[1:-1]))
+
+# easier to think about
+Point = Vector
+
+
+class Circle(object):
+    def __str__(self):
+        return "radius:{} center:{}".format(self.radius, self.center)
+
+    def __init__(self, *args):
+        if len(args) == 3 and all([isinstance(p, Point) for p in args]):
+            """ Complex solution for the radius of a circle from 3 points returns radius, centerx, centery """
+            if Vector.collinear(*args):
+                raise ValueError("v.Points used to init circle are collinear")
+            p1, p2, p3 = [complex(*list(p)) for p in args]
+            for i in range(3):
+                try:
+                    p1, p2, p3 = p3, p1, p2
+                    diff = p3 - p1
+                    diff /= p2 - p1
+                    center = (p1 - p2) * (diff - abs(diff) ** 2) / 2j / diff.imag - p1
+                    self.center = Point(center.real, center.imag)
+                except ZeroDivisionError:
+                    pass
+            self.radius = abs(complex(*self.center) + p1)
+            self.x = self.center.x
+            self.y = self.center.y
+        elif len(args) == 3:
+            self.radius = args[0]
+            self.x = args[1]
+            self.y = args[2]
+            self.center = Point(self.x, self.y)
+        else:
+            self.radius = args[0]
+            self.center = Point(*args[1])
+            self.x = self.center.x
+            self.y = self.center.y
+        self.a, self.b, self.c = self.x, self.y, self.z
+
+    def chord_angle(self, chord):
+        return 2 * np.arcsin(chord/(2*self.radius))
+
+    def intersection(self, other):
+        """ 
+        returns the points of overlap between 2 circles at a given distance
+        """
+        d = abs(self.center - other.center)
+        if self.center == other.center:
+            raise ValueError("Circles have the same center")
+        if d > self.radius + other.radius:
+            raise ValueError("Circles do not touch")
+        mid_dst = (self.radius ** 2 - other.radius ** 2 + d ** 2) / (2 * d)
+        relative_pt = (other.center - self.center) * (mid_dst / d)
+        mid_pt = self.center + relative_pt
+        chord_len = 2 * (abs(self.radius ** 2 - mid_dst ** 2)) ** .5
+        slope = chord_len / (2 * d)
+        inter_pt = Point(mid_pt.x + slope * (other.y - self.y), mid_pt.y - slope * (other.x - self.x))
+        inter_pt2 = Point(mid_pt.x - slope * (other.y - self.y), mid_pt.y + slope * (other.x - self.x))
+        return inter_pt, inter_pt2
+
+    def arc_length(self, p1, p2):
+        central_angle = self.chord_angle(p1.distance(p2))
+        return central_angle * self.radius
+
+    def plt_circle(self, color='blue'):
+        return plt.Circle(self.center, self.radius, color=color)
+
+
+class Plane(object):
+
+    def __init__(self, *args):
+
+        if len(args) == 3 and all(isinstance(p, Point) for p in args):
+            vec1 = args[0] - args[1]
+            vec2 = args[1] - args[2]
+
+            self.cross_product = vec1 @ vec2
+            self.a, self.b, self.c = self.cross_product
+            self.d = -1 * (self.cross_product * args[0])
+            # equation of plane: ax+by+cz+d=0
+
+            # for mapping points to 2d space
+            self.center = (args[0])
+
+        elif len(args) == 4:
+                self.cross_product = Vector(*args[:3])
+                self.d = args[4]
+        else:
+            raise TypeError("Arguments must be either 3 Points or 4 coefficients a, b, c, d")
+
+    def closest(self, point):
+        t = -(self.cross_product * point + self.d) / (abs(self.cross_product) ** 2)
+        return (self.cross_product * t) + point
+
+    def distance(self, point):
+        if isinstance(point, Point):
+            return abs((self.cross_product * point) + self.d) / abs(self.cross_product)
+
+    def project(self, point):
+        self.center.distance(self.closest(Point))
+
+    def __str__(self):
+        return "Plane with equation: {}x + {}y + {}z + {} = 0".format(*self.cross_product, self.d)
+
