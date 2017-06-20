8ac30dfe469c450c39122446765fec8c5085d71d
Varun Iyer <vi.mail@protonmail.ch>
2017-06-20 14:06:34 -0700

    Works!
    It sometimes draws random not-right circles but apart from that its
    pretty fine.

--- calc_arc.py
+++ calc_arc.py
@@ -7,6 +7,34 @@ from random import uniform
 from geometry import Plane, Vector, Circle, Point
 
 
+def arc3d(start, p2, end, num_points):
+    """ returns num_points points on the 3d arc containing p1, p2, and p3, starting at p1 and ending at p3"""
+    plane = Plane(start, p2, end)
+    pr1 = plane.project(start)
+    pr2 = plane.project(p2)
+    pr3 = plane.project(end)
+    circle = Circle(pr1, pr2, pr3)
+
+    radian_len = circle.chord_angle(pr1.distance(pr3))
+    increment = radian_len / num_points
+    diff = pr1 - circle.center
+    offset = np.arctan(diff.y / diff.x)
+    points3d = []
+    points3d.append(start)
+    for p in range(0, num_points-3):
+        radian = increment * p + offset
+        point = circle.center + Vector(np.sin(radian) * circle.radius, np.cos(radian) * circle.radius)
+        points3d.append(plane.unproject(point))
+    points3d.append(end)
+    return points3d
+
+
+def points_to_plt(l_points):
+    """ Takes a list of points and returns a list containing a list of each dimension """
+    x, y, z = zip(*l_points)
+    return x, y, z
+
+
 def calc_times(emitters, middles, detectors, sphere_radius, ref_index):
     """
     :param emitters: list of points of all x-ray emitters
@@ -26,6 +54,9 @@ def calc_times(emitters, middles, detectors, sphere_radius, ref_index):
                 plane = Plane(start, middle, end)
 
                 start_projection = plane.project(start)
+                # FIXME for some reason just the middle point is flipped on the y axis.
+                # Who knows why, but somebody should find out
+                # middle_projection = plane.project(Point(middle.x, -middle.y, middle.z))
                 middle_projection = plane.project(middle)
                 end_projection = plane.project(end)
 
@@ -48,7 +79,7 @@ def calc_times(emitters, middles, detectors, sphere_radius, ref_index):
                 l1 = large.arc_length(start_projection, e_pt)
                 l2 = large.arc_length(e_pt, r_pt)
                 l3 = large.arc_length(r_pt, end_projection)
-                times.append(((l1, l2 * ref_index, l3), start, middle, end))
+                times.append(((l1, l2 * ref_index, l3), start, middle, end, plane.unproject(e_pt), plane.unproject(r_pt)))
     return times
 
 
@@ -56,8 +87,8 @@ sphere_radius = 6
 index_refraction = .2
 
 emitters = [Point(-10, 0, 0)]
-detectors = [Point(0, 4, 0)]
-receivers = [Point(10, uniform(-5, 5), uniform(-5, 5)) for i in range(20)]
+detectors = [Point(0, uniform(-5, 5), uniform(-5, 5)) for i in range(10)]
+receivers = [Point(10, uniform(-5, 5), uniform(-5, 5)) for i in range(10)]
 
 times = calc_times(emitters, detectors, receivers, sphere_radius, index_refraction)
 max_time = max([sum(t[0]) for t in times])
@@ -66,12 +97,12 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # Draw arcs
-for time, start, middle, end in times:
-    time = sum(time)
+for lengths, start, middle, end, e_pt, r_pt in times:
+    time = sum(lengths)
     color = (time/ max_time, (max_time - time) / max_time, 0, 1)
-    ax.plot([start[0], middle[0]], [start[1], middle[1]], [start[2], middle[2]], c=color)
-    ax.plot([end[0], middle[0]], [end[1], middle[1]], [end[2], middle[2]], c=color)
-
+    ax.plot(*points_to_plt(arc3d(start, middle, end, 50)), c=color)
+    ax.scatter(*points_to_plt([e_pt, r_pt]), c=color)
+    ax.scatter(*points_to_plt([start, middle, end]), c="blue")
 
 # draw sphere
 u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
@@ -79,14 +110,6 @@ x = np.cos(u)*np.sin(v) * sphere_radius
 y = np.sin(u)*np.sin(v) * sphere_radius
 z = np.cos(v) * sphere_radius
 ax.plot_wireframe(x, y, z, color=(0, 0, .5, .1))
-
-# Draw emitters and stuff
-ex, ey, ez = zip(*emitters)
-dx, dy, dz = zip(*detectors)
-rx, ry, rz = zip(*receivers)
-ax.scatter(ex, ey, ez, c="red")
-ax.scatter(dx, dy, dz, c="blue")
-ax.scatter(rx, ry, rz, c="green")
 
 # Set up and display graph
 ax.set_ylim((-10, 10))
